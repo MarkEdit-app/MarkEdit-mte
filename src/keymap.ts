111e98dbb3cdc78311781579899b5d5b4cd80551
Gnohz Gniy <0x00eeee@gmail.com>
2024-09-16 19:56:13 +0800

    Improve keyboard event handling

--- src/keymap.ts
+++ src/keymap.ts
@@ -1,8 +1,9 @@
+import { KeyBinding } from '@codemirror/view';
 import { Alignment, TableEditor, defaultOptions as options } from '@tgrosinger/md-advanced-tables';
 
 export const keyBindings = (tableEditor: TableEditor) => tableActions(tableEditor).map(([key, action]) => ({
   key,
-  preventDefault: true,
+  preventDefault: false,
   run: () => {
     if (tableEditor.cursorIsInTable(options)) {
       action();
@@ -11,7 +12,7 @@ export const keyBindings = (tableEditor: TableEditor) => tableActions(tableEdito
 
     return false;
   },
-}));
+} as KeyBinding));
 
 const tableActions = (tableEditor: TableEditor) => Object.entries({
   'Tab': () => tableEditor.nextCell(options),
@@ -26,12 +27,50 @@ const tableActions = (tableEditor: TableEditor) => Object.entries({
   'Cmd-ArrowRight': () => tableEditor.moveFocus(0, 1, options),
   'Cmd-ArrowUp': () => tableEditor.moveFocus(-1, 0, options),
   'Cmd-ArrowDown': () => tableEditor.moveFocus(1, 0, options),
-  'Cmd-k Cmd-i': () => tableEditor.insertRow(options),
-  'Cmd-l Cmd-i': () => tableEditor.deleteRow(options),
-  'Cmd-k Cmd-j': () => tableEditor.insertColumn(options),
-  'Cmd-l Cmd-j': () => tableEditor.deleteColumn(options),
   'Alt-Shift-Cmd-ArrowLeft': () => tableEditor.moveColumn(-1, options),
   'Alt-Shift-Cmd-ArrowRight': () => tableEditor.moveColumn(1, options),
   'Alt-Shift-Cmd-ArrowUp': () => tableEditor.moveRow(-1, options),
   'Alt-Shift-Cmd-ArrowDown': () => tableEditor.moveRow(1, options),
+  // Instead of using consecutive bindings like "Cmd-k Cmd-i", we have to handle states manually.
+  // The reason is that "Cmd-k Cmd-i" stops propagation for "Cmd-k", which might be used in the app.
+  'Cmd-k': () => {
+    states.pendingInsert = true;
+    states.pendingDelete = false;
+  },
+  'Cmd-l': () => {
+    states.pendingDelete = true;
+    states.pendingInsert = false;
+  },
+  'Cmd-i': () => {
+    if (states.pendingInsert) {
+      tableEditor.insertRow(options);
+    }
+    if (states.pendingDelete) {
+      tableEditor.deleteRow(options);
+    }
+  },
+  'Cmd-j': () => {
+    if (states.pendingInsert) {
+      tableEditor.insertColumn(options);
+    }
+    if (states.pendingDelete) {
+      tableEditor.deleteColumn(options);
+    }
+  },
 });
+
+// Reset states with a general keydown observer
+document.addEventListener('keydown', event => {
+  const onlyMetaKey = event.metaKey && !(event.ctrlKey || event.altKey || event.shiftKey);
+  if (!onlyMetaKey || event.key !== 'k') {
+    states.pendingInsert = false;
+  }
+  if (!onlyMetaKey || event.key !== 'l') {
+    states.pendingDelete = false;
+  }
+});
+
+const states = {
+  pendingInsert: false,
+  pendingDelete: false,
+};
